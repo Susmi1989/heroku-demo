39a8c29a422447f6f4eccccb4d0438010e392eb1
susmi <susmi.vysakham@gmail.com>
2019-08-04 13:59:42 -0400

    controller added

--- src/main/java/com/susmi/herokudemo/controller/HomeController.java
+++ src/main/java/com/susmi/herokudemo/controller/HomeController.java
@@ -1,10 +1,12 @@
 package com.susmi.herokudemo.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 	
+	@GetMapping("/")
 	public String home(){
 		return "hello";
 	}
